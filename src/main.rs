12832d497b59a115c2bd06ac06253f477bbbb100
Iliand-DC <iluha805@gmail.com>
2023-12-01 21:29:53 +0300

    Работает с сохранением куда откуда из n-го количества картинок

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,24 @@ use std::io::BufWriter;
 use jpeg_to_pdf::JpegToPdf;
 
 fn main() {
-    let files = get_arguments();
-    create_pdf(files);
+    let (files, path) = get_arguments();
+    create_pdf(files, path);
 }
 
-fn get_arguments() -> Vec<String> {
+fn get_arguments() -> (Vec<String>, String) {
     let args: Vec<String> = env::args().collect();
 
     let mut files: Vec<String> = Vec::new();
-    for i in 1..args.len() {
+    for i in 1..args.len()-1 {
         files.push(args[i].to_string());
     }
-    files
+
+    let path = args[args.len()-1].clone();
+    (files, path)
 }
 
-fn create_pdf(jpeg_names: Vec<String>) {
-    let out_file = File::create("result.pdf")
+fn create_pdf(jpeg_names: Vec<String>, path: String) {
+    let out_file = File::create(path+"/result.pdf")
         .expect("Не могу инициализировать PDF");
     let mut files: Vec<Vec<u8>> = Vec::new();
     for item in jpeg_names {
